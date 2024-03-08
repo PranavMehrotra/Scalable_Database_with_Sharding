c6710e4af247e85643e07aeb454e212f18f62d02
Saransh Sharma <73842575+saransh03sharma@users.noreply.github.com>
2024-03-08 17:02:39 +0530

    server.py documented and commented

--- server/server.py
+++ server/server.py
@@ -1,19 +1,21 @@
 # Import necessary modules
 from aiohttp import web
-import os
 from manager import Manager
-import json
-import time
 
 mgr = Manager()  ### Add user and password here of SQL server
 server_id = ""
 
+# Config endpoint to initialize the database
 async def config(request):
     try:
         print("Config endpoint called")
-        request_json = await request.json()  
+
+        # Get the JSON data from the request
+        request_json = await request.json() 
+        # Call the Config_database method of the Manager class 
         message, status = mgr.Config_database(request_json)
 
+        # Create a response JSON
         if status == 200:
             shards = request_json.get('shards', [])
             message = ", ".join([f"{server_id}:{shard}" for shard in shards]) + " configured"
@@ -24,7 +26,7 @@ async def config(request):
             }
             
         else:
-           
+           # fail json message
             response_json = {
                 "error": str(message),
                 "status": "failure"
@@ -35,7 +37,8 @@ async def config(request):
     except Exception as e:
         print(f"Server: Error in Config endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
-    
+
+# Heartbeat endpoint to check if the server is running    
 async def heartbeat(request):
     try:
         return web.Response(status=200)
@@ -44,15 +47,19 @@ async def heartbeat(request):
         print(f"Server: Error in heartbeat endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
 
+# Copy endpoint to return database entries
 async def copy_database(request):
     try:
         print("Copy endpoint called")
+        # Get the JSON data from the request
         request_json = await request.json()         
         database_copy, status = mgr.Copy_database(request_json)
         
+        # Create a response JSON
         if status == 200:
             response_json = {}
             
+            # Remove the first column (auto increment ID column) from each row
             for shard_name, shard_data in database_copy.items():
                 updated_shard_data = [[row[1:], ] for row in shard_data]
                 response_json[shard_name] = updated_shard_data
@@ -72,12 +79,15 @@ async def copy_database(request):
         print(f"Server: Error in copy endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
 
+# Read endpoint to return database entries where Stud_id>= low and Stud_id < high
 async def read_database(request):
     try:
         print("Read endpoint called")
+        # Get the JSON data from the request
         request_json = await request.json()  
         database_entry, status = mgr.Read_database(request_json)
         
+        # Create a response JSON
         if status==200:
             umodified_list = [(item[1], item[2], item[3]) for item in database_entry]
             response_data = {
@@ -98,10 +108,12 @@ async def read_database(request):
         print(f"Server: Error in read endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
 
+# Write endpoint to add a new entry to the database
 async def write_database(request):
     
     try:
         print("Write endpoint called")
+        # Get the JSON data from the request
         request_json = await request.json()  
         message, status,id = mgr.Write_database(request_json)
         
@@ -126,14 +138,17 @@ async def write_database(request):
         print(f"Server: Error in write endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)  
 
+# Update endpoint to update an existing entry in the database
 async def update(request):
     
     try:
         print("Update endpoint called")
+        # Get the JSON data from the request
         request_json = await request.json()  
-        stud_id = request_json.get("Stud_id")
+        stud_id = request_json.get("Stud_id",[])
         message, status = mgr.Update_database(request_json)
     
+        # Create a response JSON
         if status == 200:
             response_data = {
                 "message": f"Data entry for Stud_id:{stud_id} updated",
@@ -153,10 +168,12 @@ async def update(request):
         print(f"Server: Error in update endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
 
+# Delete endpoint to remove an entry from the database
 async def del_database(request):
     
     try:
         print("Delete endpoint called")
+        # Get the JSON data from the request
         request_json = await request.json()  
         stud_id = request_json.get("Stud_id")
         message, status = mgr.Delete_database(request_json)
@@ -180,6 +197,7 @@ async def del_database(request):
         print(f"Server: Error in delete endpoint: {str(e)}")
         return web.json_response({"error": "Internal Server Error"}, status=500)
 
+# Catch-all endpoint for any other request
 async def not_found(request):
     return web.Response(text="Not Found", status=400)
 
